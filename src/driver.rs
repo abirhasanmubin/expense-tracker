6b3d78116e9b6b8e9c0822dc8d8f1b53742c0a35
abirhasanmubin <abirhasanmubin@gmail.com>
2023-12-07 18:27:52 +0600

    added category selection

--- src/driver.rs
+++ src/driver.rs
@@ -44,21 +44,7 @@ pub fn home(expense_list: &mut ExpenseList) {
 
 pub fn add_expense(expense_list: &mut ExpenseList) {
     println!("Enter expense details:");
-    let category = match take_number_input("Select Category", 1, 12) {
-        1 => ExpenseCategory::FoodAndDining,
-        2 => ExpenseCategory::Transportation,
-        3 => ExpenseCategory::Utilities,
-        4 => ExpenseCategory::Housing,
-        5 => ExpenseCategory::Entertainment,
-        6 => ExpenseCategory::Healthcare,
-        7 => ExpenseCategory::ClothingAndPersonalCare,
-        8 => ExpenseCategory::Travel,
-        9 => ExpenseCategory::Education,
-        10 => ExpenseCategory::GiftsAndDonations,
-        11 => ExpenseCategory::SavingsAndInvestments,
-        12 => ExpenseCategory::Miscellaneous,
-        _ => ExpenseCategory::Miscellaneous,
-    };
+    let category = select_category();
     let amount = take_number_input("Enter amount: ", 1, 100000);
     println!("Enter description: ");
     let description = take_input();
@@ -94,6 +80,54 @@ pub fn delete_expense(expense_list: &mut ExpenseList) {
     println!("{}", "Expense deleted successfully!".yellow());
     // call home function
     home(expense_list);
+}
+
+fn select_category() -> ExpenseCategory {
+    let category_enums = [
+        ExpenseCategory::FoodAndDining,
+        ExpenseCategory::Transportation,
+        ExpenseCategory::Utilities,
+        ExpenseCategory::Housing,
+        ExpenseCategory::Entertainment,
+        ExpenseCategory::Healthcare,
+        ExpenseCategory::ClothingAndPersonalCare,
+        ExpenseCategory::Travel,
+        ExpenseCategory::Education,
+        ExpenseCategory::GiftsAndDonations,
+        ExpenseCategory::SavingsAndInvestments,
+        ExpenseCategory::Miscellaneous,
+    ];
+    let mut str = String::new();
+    let category_enums_length = category_enums.len();
+    for (index, category) in category_enums.iter().enumerate() {
+        let category_string = format!("{}: {}", index + 1, category);
+        str.push_str(&category_string);
+        if index % 3 == 0 || index == category_enums_length - 1 {
+            str.push_str("\n");
+        } else {
+            str.push_str(" ");
+        }
+    }
+    let input = match take_number_input(
+        format!("Select a category:\n{}", str.yellow()).as_str(),
+        1,
+        12,
+    ) {
+        1 => ExpenseCategory::FoodAndDining,
+        2 => ExpenseCategory::Transportation,
+        3 => ExpenseCategory::Utilities,
+        4 => ExpenseCategory::Housing,
+        5 => ExpenseCategory::Entertainment,
+        6 => ExpenseCategory::Healthcare,
+        7 => ExpenseCategory::ClothingAndPersonalCare,
+        8 => ExpenseCategory::Travel,
+        9 => ExpenseCategory::Education,
+        10 => ExpenseCategory::GiftsAndDonations,
+        11 => ExpenseCategory::SavingsAndInvestments,
+        12 => ExpenseCategory::Miscellaneous,
+        _ => ExpenseCategory::Miscellaneous,
+    };
+    input
 }
 
 fn take_number_input(input_message: &str, minimum: u32, maximum: u32) -> u32 {
